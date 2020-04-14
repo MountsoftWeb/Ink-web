c89c9539b14ec255eba456cfb55e633316bbb68d
Carlos <458074992@qq.com>
2020-04-14 22:42:22 +0800

    测试

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,53 +1,61 @@
 <template>
   <div class="header clearfix">
-
-<el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
-  <el-menu-item index="1">处理中心</el-menu-item>
-  <el-submenu index="2">
-    <template slot="title">我的工作台</template>
-    <el-menu-item index="2-1">选项1</el-menu-item>
-    <el-menu-item index="2-2">选项2</el-menu-item>
-    <el-menu-item index="2-3">选项3</el-menu-item>
-    <el-submenu index="2-4">
-      <template slot="title">选项4</template>
-      <el-menu-item index="2-4-1">选项1</el-menu-item>
-      <el-menu-item index="2-4-2">选项2</el-menu-item>
-      <el-menu-item index="2-4-3">选项3</el-menu-item>
-    </el-submenu>
-  </el-submenu>
-  <el-menu-item index="3">消息中心</el-menu-item>
-  <el-menu-item index="4"><router-link :to="{path:'/spider'}">爬虫</router-link> </el-menu-item>
-</el-menu>
-<div class="line"></div>
-<el-menu
-  :default-active="activeIndex2"
-  class="el-menu-demo"
-  mode="horizontal"
-  @select="handleSelect"
-  background-color="#000000"
-  text-color="#fff"
-  active-text-color="#ffd04b">
-  <el-menu-item index="1">处理中心</el-menu-item>
-  <el-submenu index="2">
-    <template slot="title">我的工作台</template>
-    <el-menu-item index="2-1">选项1</el-menu-item>
-    <el-menu-item index="2-2">选项2</el-menu-item>
-    <el-menu-item index="2-3">选项3</el-menu-item>
-    <el-submenu index="2-4">
-      <template slot="title">选项4</template>
-      <el-menu-item index="2-4-1">选项1</el-menu-item>
-      <el-menu-item index="2-4-2">选项2</el-menu-item>
-      <el-menu-item index="2-4-3">选项3</el-menu-item>
-    </el-submenu>
-  </el-submenu>
-  <el-menu-item index="3" disabled>消息中心</el-menu-item>
-  <el-menu-item index="4"><a href="https://www.ele.me" target="_blank">订单管理</a></el-menu-item>
-</el-menu>
+    <el-menu
+      :default-active="activeIndex"
+      class="el-menu-demo"
+      mode="horizontal"
+      @select="handleSelect"
+    >
+      <el-menu-item index="1">处理中心</el-menu-item>
+      <el-submenu index="2">
+        <template slot="title">我的工作台</template>
+        <el-menu-item index="2-1">选项1</el-menu-item>
+        <el-menu-item index="2-2">选项2</el-menu-item>
+        <el-menu-item index="2-3">选项3</el-menu-item>
+        <el-submenu index="2-4">
+          <template slot="title">选项4</template>
+          <el-menu-item index="2-4-1">选项1</el-menu-item>
+          <el-menu-item index="2-4-2">选项2</el-menu-item>
+          <el-menu-item index="2-4-3">选项3</el-menu-item>
+        </el-submenu>
+      </el-submenu>
+      <el-menu-item index="3">消息中心</el-menu-item>
+      <el-menu-item index="4"
+        ><router-link :to="{ path: '/spider' }">爬虫</router-link>
+      </el-menu-item>
+    </el-menu>
+    <div class="line"></div>
+    <el-menu
+      :default-active="activeIndex2"
+      class="el-menu-demo"
+      mode="horizontal"
+      @select="handleSelect"
+      background-color="#000000"
+      text-color="#fff"
+      active-text-color="#ffd04b"
+    >
+      <el-menu-item index="1">处理中心</el-menu-item>
+      <el-submenu index="2">
+        <template slot="title">我的工作台</template>
+        <el-menu-item index="2-1">选项1</el-menu-item>
+        <el-menu-item index="2-2">选项2</el-menu-item>
+        <el-menu-item index="2-3">选项3</el-menu-item>
+        <el-submenu index="2-4">
+          <template slot="title">选项4</template>
+          <el-menu-item index="2-4-1">选项1</el-menu-item>
+          <el-menu-item index="2-4-2">选项2</el-menu-item>
+          <el-menu-item index="2-4-3">选项3</el-menu-item>
+        </el-submenu>
+      </el-submenu>
+      <el-menu-item index="3" disabled>消息中心</el-menu-item>
+      <el-menu-item index="4"
+        ><a href="https://www.ele.me" target="_blank">订单管理</a></el-menu-item
+      >
+    </el-menu>
   </div>
 </template>
 
 <script>
-
 //         input.onclick = function(){
 //           input.style.width = 160 +'px';
 //         }
@@ -56,31 +64,27 @@
 //         }
 //
 export default {
-    methods: {
-      logout () {
-          // alert(localStorage.getItem('token'))
+  methods: {
+    logout() {
+      // alert(localStorage.getItem('token'))
 
-          this.$store.dispatch('logOut', '').then(() => {
-          //  跳转到首页
-              this.$router.push('/login');
-          })
-          // alert(localStorage.getItem('token'))
-          // alert(this.$store.state.token)
-      }
-    },
-
-}
-
-
+      this.$store.dispatch("logOut", "").then(() => {
+        //  跳转到首页
+        this.$router.push("/login");
+      });
+      // alert(localStorage.getItem('token'))
+      // alert(this.$store.state.token)
+    }
+  }
+};
 </script>
 
 <style scoped>
-  * {
-    margin: 0;
-    padding: 0;
-  }
-  body {
-    /* height: 16000px; */
-  }
-
+* {
+  margin: 0;
+  padding: 0;
+}
+body {
+  /* height: 16000px; */
+}
 </style>
